b292de35ad59e0578a44e4d55445e2e9dd59dcf4
yash parmar <yah67238@gmail.com>
2024-08-31 13:37:19 +0530

    src/approuter.tsx and vite.cnfig.ts file updated onece

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,71 +1,76 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "./layouts/layout";
-import HomePage from "./pages/HomePage";
-import AuthCallbackPage from "./pages/AuthCallbackPage";
-import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
-import ManageRestaurantPage from "./pages/ManageRestaurantPage";
-import SearchPage from "./pages/SearchPage";
-import DetailPage from "./pages/DetailPage";
-import OrderStatusPage from "./pages/OrderStatusPage";
+
+// Dynamic imports for pages
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AuthCallbackPage = lazy(() => import("./pages/AuthCallbackPage"));
+const UserProfilePage = lazy(() => import("./pages/UserProfilePage"));
+const ManageRestaurantPage = lazy(() => import("./pages/ManageRestaurantPage"));
+const SearchPage = lazy(() => import("./pages/SearchPage"));
+const DetailPage = lazy(() => import("./pages/DetailPage"));
+const OrderStatusPage = lazy(() => import("./pages/OrderStatusPage"));
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout showHero>
-            <HomePage />
-          </Layout>
-        }
-      />
-      <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/search/:city"
-        element={
-          <Layout showHero={false}>
-            <SearchPage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/detail/:restaurantId"
-        element={
-          <Layout showHero={false}>
-            <DetailPage />
-          </Layout>
-        }
-      />
-      <Route element={<ProtectedRoute />}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
         <Route
-          path="/order-status"
+          path="/"
           element={
-            <Layout>
-              <OrderStatusPage />
+            <Layout showHero>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route
+          path="/search/:city"
+          element={
+            <Layout showHero={false}>
+              <SearchPage />
             </Layout>
           }
         />
         <Route
-          path="/user-profile"
+          path="/detail/:restaurantId"
           element={
-            <Layout>
-              <UserProfilePage />
+            <Layout showHero={false}>
+              <DetailPage />
             </Layout>
           }
         />
-        <Route
-          path="/manage-restaurant"
-          element={
-            <Layout>
-              <ManageRestaurantPage />
-            </Layout>
-          }
-        />
-      </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/order-status"
+            element={
+              <Layout>
+                <OrderStatusPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <Layout>
+                <UserProfilePage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/manage-restaurant"
+            element={
+              <Layout>
+                <ManageRestaurantPage />
+              </Layout>
+            }
+          />
+        </Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // ensure this matches your expected output directory
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    chunkSizeWarningLimit: 500, // you can adjust this limit as needed
   },
 });
