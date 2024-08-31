efeceb84d5ca585dc4bb7f0aedb5b08e2f5445cc
yash parmar <yah67238@gmail.com>
2024-08-31 13:29:05 +0530

    new changes apply

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    outDir: 'dist', // ensure this matches your expected output directory
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     },
+    chunkSizeWarningLimit: 500, // you can adjust this limit as needed
   },
 });
